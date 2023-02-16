037cd5e6d21892dab04d3b305a76f4a198917cfa
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-02-16 14:11:59 +0000

    Added branch with code to download package.jsons/locks from repos

--- run.py
+++
@@ -1,5 +0,0 @@
-#!/usr/env python3
-
-from importlib import reload as r
-from dep_checker import dep_checker as d
-
--- test/test_dep_checker.py
+++
@@ -1,9 +0,0 @@
-#!/usr/env python3
-
-import unittest
-from dep_checker import dep_checker
-
-class DummyTest(unittest.TestCase):
-
-    def test_a(self):
-        self.assertTrue(dep_checker.test_func())
